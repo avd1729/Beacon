e3bebf370317577f3c49505a4de4d847344d1db4
Aravind <aravind19112003@gmail.com>
2025-07-12 13:27:09 +0530

    init db for peer registry

---
+++ models/bootstrap_server.py
@@ -0,0 +1,2 @@
+class BootstrapServer:
+    pass
---
+++ src/database.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+conn = sqlite3.connect('db/peer_registry.db')
+
+cursor = conn.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    ip TEXT NOT NULL,
+    port TEXT NOT NULL,
+    UNIQUE(ip, port)
+)
+''')
+
+conn.commit()
+conn.close()
