6966d1da1eb6ffe7f0f27e44d971c1bfbebcd30f
Aravind <aravind19112003@gmail.com>
2025-07-13 16:08:54 +0530

    add basic test & CI

--- tests/test_peer.py
+++ tests/test_peer.py
@@ -4,7 +4,7 @@ from models.peer import Peer
 class TestPeer(unittest.TestCase):
     def test_peer_creation(self):
         peer = Peer("127.0.0.1", 5000)
-        self.assertEqual(peer.host, "127.0.0.1")
+        self.assertEqual(peer.ip, "127.0.0.1")
         self.assertEqual(peer.port, 5000)
 
 if __name__ == '__main__':
